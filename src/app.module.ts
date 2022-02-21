ea27652f2b35ce5b8daebcded75be471895a6e54
VyVu <vyvc@comartek.vn>
2022-02-21 23:27:11 +0700

    Create NestJS app

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatController } from './cats/cat.controller';
+import { CatService } from './cats/cat.service';
+
+@Module({
+	imports: [],
+	controllers: [AppController, CatController],
+	providers: [AppService, CatService],
+})
+export class AppModule {}
